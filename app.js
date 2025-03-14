6127d947db5a47f9479a19f2749fca29b0f1ed94
Kareem Farrell <kareemfarrellonline@gmail.com>
2025-03-13 20:35:47 -0400

    Added a Light and Dark Mode

--- app.js
+++ app.js
@@ -84,3 +84,27 @@ function getTodos() {
     return JSON.parse(todos);
 }
 
+
+// Light Mode
+
+let lightmode = localStorage.getItem('lightmode')
+const themeSwitch = document.querySelector("#theme-switch")
+
+const enableLightmode = () => {
+    document.body.classList.add('light-mode')
+    localStorage.setItem('lightmode', 'active');
+}
+
+const disableLightmode = () => {
+    document.body.classList.remove('light-mode')
+    localStorage.setItem('lightmode', 'null');
+}
+
+if (lightmode === "active") {
+    enableLightmode();
+}
+
+themeSwitch.addEventListener("click", () => {
+    lightmode = localStorage.getItem('lightmode')
+    lightmode !== "active" ? enableLightmode() : disableLightmode()
+})
